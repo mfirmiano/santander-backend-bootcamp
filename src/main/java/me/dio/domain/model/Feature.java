e2251b9ce8c6d8654cff4d0900ab4c672f1bd39e
mfirmiano <mfirmiano1902@gmail.com>
2023-10-17 03:06:39 -0300

    Implementação da camada de domínio

---
+++ src/main/java/me/dio/domain/model/Feature.java
@@ -0,0 +1,9 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tabela_feature")
+public class Feature extends BasicItem{
+
+
+}
---
+++ src/main/java/me/dio/domain/model/News.java
@@ -0,0 +1,9 @@
+package me.dio.domain.model;
+
+import jakarta.persistence.Entity;
+
+@Entity(name = "tabela_news")
+
+public class News extends BasicItem{
+
+}
